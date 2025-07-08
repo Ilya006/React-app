2725cbb46445a4c3f9e09420e7953c130fb864ed
ilya Senognoev <ilya.senognoev@finnplay.com>
2025-07-08 20:56:48 +0500

    Added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,24 @@ import { defineConfig } from 'vite'
 import svg from '@neodx/svg/vite';
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), svgr(), svg({
-    group: true,
-    root: 'src/shared/ui/icon/assets',
-    output: 'public/sprite',
-    resetColors: false,
-    metadata: 'src/shared/ui/icon/sprite.h.ts',
-  })],
+  plugins: [
+    react(),
+    svgr(),
+    tailwindcss(),
+    svg({
+      group: true,
+      root: 'src/shared/ui/icon/assets',
+      output: 'public/sprite',
+      resetColors: false,
+      metadata: 'src/shared/ui/icon/sprite.h.ts',
+    })
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
